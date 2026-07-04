3d17251180bb72a859e902fd0e4a250d8acb8ebe
ataur-abir <sheikh.abir477@gmail.com>
2026-07-04 23:10:27 +0600

    update js file

---
+++ script.js
@@ -0,0 +1,9 @@
+// Buy Now Button
+document.getElementById("buyBtn").onclick = function () {
+    alert("Thank you for purchasing from AudioMart. ");
+};
+
+// Contact Us Button
+document.getElementById("contactBtn").onclick = function () {
+    alert("Contact us through mail.");
+};
